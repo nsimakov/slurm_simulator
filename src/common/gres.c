8cf42f5a633120ac5c357a4b0bcbc38c8a10a0fc
Nikolay Simakov <nikolays@buffalo.edu>
2017-02-22 13:50:15 -0500

    set gres_cnt to gres_data->gres_cnt_config in order to register nodes with gres in simulation mode

--- src/common/gres.c
+++ src/common/gres.c
@@ -1896,7 +1896,13 @@ extern int _node_config_validate(char *node_name, char *orig_config,
 	if (gres_data->node_feature)
 		return rc;
 
+#ifdef SLURM_SIMULATOR
+	gres_cnt = gres_data->gres_cnt_config;
+#else
 	gres_cnt = _get_tot_gres_cnt(context_ptr->plugin_id, &set_cnt);
+#endif
+
+
 	if (gres_data->gres_cnt_found != gres_cnt) {
 		if (gres_data->gres_cnt_found != NO_VAL64) {
 			info("%s: count changed for node %s from %"PRIu64" "
