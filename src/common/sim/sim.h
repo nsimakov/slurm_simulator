42f3ed077bdf76a06d07409f42c8a01e270043e7
Nikolay Simakov <ns@compmodel.org>
2017-03-12 22:01:58 -0400

    updated trace to use pointers to chars, added dependencies handling and initial support for job cancellation

--- src/common/sim/sim.h
+++ src/common/sim/sim.h
@@ -72,39 +72,34 @@ extern sem_t * mutexsignal;
 /******************************************************************************
  * Simulator job traces
  ******************************************************************************/
-#define MAX_USERNAME_LEN 30
-#define MAX_RSVNAME_LEN  30
-#define MAX_QOSNAME      30
-#define TIMESPEC_LEN     30
-#define MAX_RSVNAME      30
-
 typedef struct job_trace {
-	int  job_id;
-	char username[MAX_USERNAME_LEN];
-	long int submit; /* relative or absolute? */
-	int  duration;
-	int  wclimit;
-	int  tasks;
-	char qosname[MAX_QOSNAME];
-	char partition[MAX_QOSNAME];
-	char account[MAX_QOSNAME];
-	int  cpus_per_task;
-	int  tasks_per_node;
-	char reservation[MAX_RSVNAME];
-	char dependency[MAX_RSVNAME];
-	uint64_t pn_min_memory;/* minimum real memory (in MB) per node OR
-	  * real memory per CPU | MEM_PER_CPU,
-	  * NO_VAL use partition default,
-	  * default=0 (no limit) */
-	char features[MAX_RSVNAME];
-	char gres[MAX_RSVNAME];
-	int shared;/* 2 if the job can only share nodes with other
-	 *   jobs owned by that user,
-	 * 1 if job can share nodes with other jobs,
-	 * 0 if job needs exclusive access to the node,
-	 * or NO_VAL to accept the system default.
-	 * SHARED_FORCE to eliminate user control. */
-	struct job_trace *next;
+    int  job_id;
+    char *username;
+    long int submit; /* relative or absolute? */
+    int  duration;
+    int  wclimit;
+    int  tasks;
+    char *qosname;
+    char *partition;
+    char *account;
+    int  cpus_per_task;
+    int  tasks_per_node;
+    char *reservation;
+    char *dependency;
+    uint64_t pn_min_memory;/* minimum real memory (in MB) per node OR
+     * real memory per CPU | MEM_PER_CPU,
+     * NO_VAL use partition default,
+     * default=0 (no limit) */
+    char *features;
+    char *gres;
+    int shared;/* 2 if the job can only share nodes with other
+     *   jobs owned by that user,
+     * 1 if job can share nodes with other jobs,
+     * 0 if job needs exclusive access to the node,
+     * or NO_VAL to accept the system default.
+     * SHARED_FORCE to eliminate user control. */
+    long int cancelled; /* time when job should be cancelled, 0 if never */
+    struct job_trace *next;
 } job_trace_t;
 
 extern job_trace_t *trace_head;
@@ -121,11 +116,20 @@ extern job_trace_t *in_queue_trace_tail;
 /* insert job to in_queue_trace */
 extern int insert_in_queue_trace_record(job_trace_t *new);
 
+/* insert job to in_queue_trace */
+extern int remove_from_in_queue_trace_record(job_trace_t *remove);
+
 /* find job in_queue_trace */
 extern job_trace_t* find_job__in_queue_trace_record(int job_id);
 /******************************************************************************
  * Simulator Events
  ******************************************************************************/
+/*typedef enum simulator_event_type {
+	SIM_JOB_AND_EPILOG_COMPLETE,
+	SIM_JOB_COMPLETE,
+	SIM_EPILOG_COMPLETE,
+	SIM_CANCEL_JOB
+} simulator_event_type_t;*/
 
 typedef struct simulator_event {
     uint32_t job_id;
